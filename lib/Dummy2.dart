83aa1c822f896bb15f6eee6a07067e3f42a9d826
niiitish1 <71102495+niiitish1@users.noreply.github.com>
2021-10-10 00:38:57 +0530

    done

--- lib/Dummy2.dart
+++ lib/Dummy2.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  print("hello nitish");
+  runApp(MaterialApp(
+    title: 'hello',
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                height: 200.00,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 200,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100,
+                height: 200,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
