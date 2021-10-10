ade4426b76134305d13990c5963fa0c649c2a8b0
sakshigothal <sakshigothal302000@gmail.com>
2021-10-10 12:41:38 +0530

    make some changes

--- lib/Dummy2.dart
+++ lib/Dummy2.dart
@@ -19,28 +19,41 @@ class MyApp extends StatelessWidget {
         child: Container(
           width: double.infinity,
           height: double.infinity,
-          color: Colors.white,
-          child: Row(
+          color: Colors.green,
+          child: Column(
             children: [
-              Container(
-                width: 100,
-                height: 200.00,
-                color: Colors.red,
-              ),
-              Container(
-                width: 100,
-                height: 200,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 100,
-                height: 200,
-                color: Colors.green,
-              ),
+              Commonrow(Colors.red, clr3: Colors.white, clr2: Colors.amber)
+              // Commonrow(Colors.red, Colors.green, Colors.blue),
+              // Commonrow(Colors.blue, Colors.red, Colors.green),
+              // Commonrow(Colors.green, Colors.blue, Colors.red),
+              // Commonrow(Colors.blue, clr2:Colors.black12, clr3: Colors.brown)
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget Commonrow(Color clr1,
+    {Color? clr2 = Colors.blue, Color clr3 = Colors.black}) {
+  return Row(
+    children: [
+      Container(
+        width: 100,
+        height: 200.00,
+        color: clr1,
+      ),
+      Container(
+        width: 100,
+        height: 200,
+        color: clr2,
+      ),
+      Container(
+        width: 100,
+        height: 200,
+        color: clr3,
+      ),
+    ],
+  );
 }
