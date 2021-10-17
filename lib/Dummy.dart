72eccf275803653bfa4b820a97c21476f51e035d
sakshigothal <sakshigothal302000@gmail.com>
2021-10-17 13:08:02 +0530

    1710 assignment

--- lib/Dummy.dart
+++ lib/Dummy.dart
@@ -1,12 +1,173 @@
 // ignore_for_file: file_names
-import 'dart:js';
 
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MaterialApp(
-//     title: 'nitish',
-//     home: IndiaFlag(),
-//   ));
-// }
+class Demo extends StatefulWidget {
+  const Demo({Key? key}) : super(key: key);
 
+  @override
+  _DemoState createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              AppBar(
+                title: Text("Account"),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.search),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.home),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              buildSigupBox(),
+              SizedBox(
+                height: 10,
+              ),
+              moreDetails(Icons.shop, "My Followed Shop"),
+              moreDetails(Icons.bakery_dining, "My Bank Details"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+              moreDetails(Icons.dangerous, "My Followed Shop"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildSigupBox() {
+  return Container(
+    color: Colors.white,
+    child: Row(
+      children: [
+        Icon(
+          Icons.person,
+          color: Colors.grey,
+          size: 70,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Sign Up"),
+            ),
+            Text("View and update your profile details"),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget moreDetails(IconData icon, String text) {
+  return Container(
+    color: Colors.white,
+    child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Icon(icon),
+              SizedBox(
+                width: 20,
+              ),
+              Text(text),
+            ],
+          ),
+        ),
+        Container(
+          height: 10,
+          color: Colors.grey,
+        )
+      ],
+    ),
+  );
+}
+
+class Rashi extends StatefulWidget {
+  const Rashi({Key? key}) : super(key: key);
+
+  @override
+  _RashiState createState() => _RashiState();
+}
+
+class _RashiState extends State<Rashi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mypp"),
+        backgroundColor: Colors.yellow,
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.all(8),
+        child: GridView.builder(
+          itemCount: images.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 80,
+            mainAxisSpacing: 100,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return Image.network(images[index]);
+          },
+        ),
+      )),
+    );
+  }
+
+  List<String> images = [
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png"
+  ];
+}
+
+class Choice {
+  Choice({this.title, this.icon});
+  String? title;
+  IconData? icon;
+}
+
+List<Choice> choices = <Choice>[
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+  Choice(icon: Icons.add, title: "nitish"),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:demo_1/03_Login.dart';
+import 'package:demo_1/Dummy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -8,14 +9,17 @@ void main() {
   // ignore: prefer_const_constructors
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const LoginPage(),
+    home: const Rashi(),
   ));
   // SystemChrome.setSystemUIOverlayStyle(
   //     SystemUiOverlayStyle(systemNavigationBarColor: Colors.green));
-}
+}      
+
+  
 
 class MultiBox extends StatelessWidget {
   const MultiBox({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +33,7 @@ class MultiBox extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            /**
+            /**                                 
              * First ROW
              */
             Container(
